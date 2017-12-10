a848899b40c1ac050b0937e8418bde04d4c43673
GilBr <gilb.osm@zaclys.et>
2017-12-10 18:20:21 +0100

    Edition scripts.js et notes Ch3

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-alert("scripts.js chargé");
+//console.log("scripts.js chargé");
+//$("#response").html("scripts.js chargé");
+// Déclaration variable
+let userInput;
+userInput = "chocolat";
+// Boucle if sur valeur chocolat
+if ( userInput === "chocolat") {
+  $("#response").html("Super !");
+} else {
+  $("#response").html("Pourquoi pas du chocolat ?");
+}
