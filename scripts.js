2778cd4654ddf4665cb128c7aff6d14d15996810
GilBr <gilb.osm@zaclys.et>
2017-12-10 21:39:11 +0100

    Ch3. Compléments notes

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@
 //$("#response").html("scripts.js chargé");
 // Déclaration variable
 let userInput;
-userInput = "chocolat";
+// Affichage boite de saisie pour variable
+userInput = prompt("Quelle est votre douceur préférée ?","Répondre ici");
 // Boucle if sur valeur chocolat
 if ( userInput === "chocolat") {
   $("#response").html("Super !");
